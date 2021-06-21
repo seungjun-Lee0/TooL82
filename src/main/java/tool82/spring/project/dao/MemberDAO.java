6cb40e3c18b88e89bf6db1c9bda41843d9ee367c
Law Hope . K <80802756+LawHope@users.noreply.github.com>
2021-06-21 10:01:33 +0900

    로그인/회원가입

--- src/main/java/tool82/spring/project/dao/MemberDAO.java
+++ src/main/java/tool82/spring/project/dao/MemberDAO.java
@@ -9,6 +9,6 @@ public interface MemberDAO {
     int insertMember(Member m);
 
     int selectOneUserid(String uid);
-    int selectLogin(Member m); //멤버 정보를 토대로 매치하고 조사하는 거니까
+    int selectLogin(Member m);
 
 }
