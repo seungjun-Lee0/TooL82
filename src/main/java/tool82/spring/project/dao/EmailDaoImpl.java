e09e14b8e9f4354d07148ae6948f92a4044c6499
soonj <dhdlatl2@gmail.com>
2021-06-27 18:07:56 +0900

    주기적 이메일 전송 & 상태 변경

---
+++ src/main/java/tool82/spring/project/dao/EmailDaoImpl.java
@@ -0,0 +1,44 @@
+package tool82.spring.project.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import tool82.spring.project.vo.Buylist;
+import tool82.spring.project.vo.Member;
+import tool82.spring.project.vo.Product;
+
+import java.util.List;
+
+@Repository("emldao")
+public class EmailDaoImpl implements EmailDAO {
+
+    @Autowired
+    private SqlSession sqlSession;
+
+    @Override
+    public List<Member> selectEmail(String buystat) {
+        return sqlSession.selectList("email.selectEmail", buystat);
+    }
+
+    @Override
+    public int updateSussStat() {
+        return sqlSession.update("email.updateSussStat");
+    }
+
+    @Override
+    public int updateFailStat() {
+        return sqlSession.update("email.updateFailStat");
+    }
+
+    @Override
+    public int updateSussBuyStat() {
+        return sqlSession.update("email.updateSussBuy");
+    }
+
+    @Override
+    public int updateFailBuyStat() {
+        return sqlSession.update("email.updateFailBuy");
+    }
+
+
+}
